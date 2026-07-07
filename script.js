dedc7d8549fd62551ed336cf14d2dd1ceda6c2ac
ahmedsamir986 <architect.ahmedsamir@gmail.com>
2026-07-07 10:08:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,10 @@ requestAnimationFrame(update);
 update();
 
 });
+window.addEventListener("scroll",function(){
+
+const nav=document.querySelector(".navbar");
+
+nav.classList.toggle("scrolled",window.scrollY>80);
+
+});
